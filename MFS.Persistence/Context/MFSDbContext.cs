fade2b1e0d865f48e1c0bb92771de82b07946e8a
sherviniv@gmail.com <sherviniv@gmail.com>
2021-06-04 16:53:20 +0330

    add project infra

---
+++ MFS.Persistence/Context/MFSDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFS.Persistence.Context
+{
+    public class MFSDbContext : DbContext
+    {
+        public MFSDbContext() : base("name=MFSContextConnectionString")
+        {
+        }
+    }
+}
