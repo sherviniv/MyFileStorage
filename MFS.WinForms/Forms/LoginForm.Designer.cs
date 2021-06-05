a7100b6a9d60bac2ee49f8866b6f9b156819dbba
sherviniv@gmail.com <sherviniv@gmail.com>
2021-06-05 18:49:49 +0330

    fix loginform index

--- MFS.WinForms/Forms/LoginForm.Designer.cs
+++ MFS.WinForms/Forms/LoginForm.Designer.cs
@@ -30,7 +30,6 @@ namespace MFS.WinForms.Forms
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.picGradient = new System.Windows.Forms.PictureBox();
             this.lbLogin = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -43,28 +42,21 @@ namespace MFS.WinForms.Forms
             this.lblVersion = new System.Windows.Forms.Label();
             this.chkRemmeberMe = new System.Windows.Forms.CheckBox();
             this.lblRegister = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.picGradient)).BeginInit();
+            this.picGradient = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGradient)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // picGradient
-            // 
-            this.picGradient.BackgroundImage = global::MFS.WinForms.Properties.Resources.leftside;
-            this.picGradient.Dock = System.Windows.Forms.DockStyle.Left;
-            this.picGradient.Image = global::MFS.WinForms.Properties.Resources.leftside;
-            this.picGradient.Location = new System.Drawing.Point(0, 0);
-            this.picGradient.Name = "picGradient";
-            this.picGradient.Size = new System.Drawing.Size(192, 450);
-            this.picGradient.TabIndex = 0;
-            this.picGradient.TabStop = false;
             // 
             // lbLogin
             // 
             this.lbLogin.AutoSize = true;
             this.lbLogin.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(86)))), ((int)(((byte)(226)))));
-            this.lbLogin.Location = new System.Drawing.Point(232, 26);
+            this.lbLogin.Location = new System.Drawing.Point(232, 33);
             this.lbLogin.Name = "lbLogin";
             this.lbLogin.Size = new System.Drawing.Size(112, 47);
             this.lbLogin.TabIndex = 1;
@@ -119,7 +111,7 @@ namespace MFS.WinForms.Forms
             this.panel2.Name = "panel2";
             this.panel2.Padding = new System.Windows.Forms.Padding(8, 6, 8, 5);
             this.panel2.Size = new System.Drawing.Size(414, 38);
-            this.panel2.TabIndex = 1;
+            this.panel2.TabIndex = 2;
             // 
             // txtPassword
             // 
@@ -130,7 +122,7 @@ namespace MFS.WinForms.Forms
             this.txtPassword.Margin = new System.Windows.Forms.Padding(5);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(398, 26);
-            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TabIndex = 0;
             // 
             // btnLogin
             // 
@@ -165,10 +157,10 @@ namespace MFS.WinForms.Forms
             // 
             this.lblVersion.AutoSize = true;
             this.lblVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVersion.Location = new System.Drawing.Point(236, 417);
+            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(237, 421);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(99, 21);
+            this.lblVersion.Size = new System.Drawing.Size(82, 17);
             this.lblVersion.TabIndex = 1;
             this.lblVersion.Text = "Version 0.1.3";
             // 
@@ -193,8 +185,41 @@ namespace MFS.WinForms.Forms
             this.lblRegister.Location = new System.Drawing.Point(575, 417);
             this.lblRegister.Name = "lblRegister";
             this.lblRegister.Size = new System.Drawing.Size(109, 21);
-            this.lblRegister.TabIndex = 1;
+            this.lblRegister.TabIndex = 6;
             this.lblRegister.Text = "Register Now";
+            // 
+            // picGradient
+            // 
+            this.picGradient.BackgroundImage = global::MFS.WinForms.Properties.Resources.leftside;
+            this.picGradient.Dock = System.Windows.Forms.DockStyle.Left;
+            this.picGradient.Image = global::MFS.WinForms.Properties.Resources.leftside;
+            this.picGradient.Location = new System.Drawing.Point(0, 0);
+            this.picGradient.Name = "picGradient";
+            this.picGradient.Size = new System.Drawing.Size(192, 450);
+            this.picGradient.TabIndex = 0;
+            this.picGradient.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(228)))), ((int)(((byte)(253)))));
+            this.pictureBox1.Image = global::MFS.WinForms.Properties.Resources.mfs;
+            this.pictureBox1.Location = new System.Drawing.Point(602, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(86)))), ((int)(((byte)(226)))));
+            this.label3.Location = new System.Drawing.Point(612, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "MFS";
             // 
             // LoginForm
             // 
@@ -202,6 +227,7 @@ namespace MFS.WinForms.Forms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(228)))), ((int)(((byte)(253)))));
             this.ClientSize = new System.Drawing.Size(696, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.chkRemmeberMe);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
@@ -211,6 +237,7 @@ namespace MFS.WinForms.Forms
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lbLogin);
             this.Controls.Add(this.picGradient);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
@@ -219,11 +246,12 @@ namespace MFS.WinForms.Forms
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.picGradient)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGradient)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -244,5 +272,7 @@ namespace MFS.WinForms.Forms
         private System.Windows.Forms.Label lblVersion;
         private System.Windows.Forms.CheckBox chkRemmeberMe;
         private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- MFS.WinForms/Properties/Resources.Designer.cs
+++ MFS.WinForms/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace MFS.WinForms.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap mfs {
+            get {
+                object obj = ResourceManager.GetObject("mfs", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
